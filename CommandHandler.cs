b70b85b66beeea4b0c9ae451f7aa15b9eb525ea8
SuperGamerTron <45374546+SuperGamerTron@users.noreply.github.com>
2021-07-02 13:44:13 -0600

    Add files via upload

---
+++ CommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using MuckMod;
+using MuckMod.Patches;
+
+namespace InstantEat
+{
+    public class CommandHandler
+    {
+        [MuckCommand]
+        public static void InstantEat(string[] args)
+        {
+            Eat.configInstantEatEnabled.Value = !Eat.configInstantEatEnabled.Value;
+            ChatCommand.SendServerMessage($"Instant Eat {(Eat.configInstantEatEnabled.Value ? "Enabled" : "Disabled")}");
+        }
+    }
+}
