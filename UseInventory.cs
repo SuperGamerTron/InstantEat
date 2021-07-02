51876953e267d864099bb74d75ddd71e0a05ecb4
SuperGamerTron <45374546+SuperGamerTron@users.noreply.github.com>
2021-07-02 13:44:56 -0600

    Add files via upload

---
+++ UseInventory.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+
+namespace InstantEat.Patches
+{
+    public partial class MuckPatch
+    {
+        [HarmonyPrefix, HarmonyPatch(typeof(UseInventory), nameof(UseInventory.Use))]
+        public static bool OnUse()
+        {
+            if (Eat.currentItem && Eat.configInstantEatEnabled.Value) return !(Eat.currentItem.tag == InventoryItem.ItemTag.Food);
+            return true;
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(UseInventory), nameof(UseInventory.SetWeapon))]
+        public static void OnSetWeapon(ref InventoryItem item)
+        {
+            Eat.currentItem = item;
+        }
+    }
+}
