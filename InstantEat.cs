0c9b8f4dbf72faec0a31736b25de5739b5ae3709
SuperGamerTron <45374546+SuperGamerTron@users.noreply.github.com>
2021-07-02 16:01:53 -0600

    Add files via upload

---
+++ InstantEat.cs
@@ -0,0 +1,60 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using InstantEat.Patches;
+using System.Collections;
+using UnityEngine;
+
+namespace InstantEat
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    [BepInProcess("Muck.exe")]
+    public class InstantEat : BaseUnityPlugin
+    {
+        public static ConfigEntry<bool> configInstantEatEnabled;
+
+        public static InventoryItem currentItem;
+        public static ParticleSystem.EmissionModule eatingEmission;
+
+        public const string
+            GUID = "InstantEat",
+            NAME = "InstantEat",
+            VERSION = "1.0.0";
+
+        public void Awake()
+        {
+            configInstantEatEnabled = Config.Bind("General", "Enabled", true, "Whether or not InstantEat is enabled");
+            Harmony.CreateAndPatchAll(typeof(MuckPatch));
+            Logger.LogMessage("Loaded InstantEat");
+        }
+
+        public void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (currentItem != null && !InventoryUI.Instance.gameObject.activeInHierarchy)
+                {
+                    if (configInstantEatEnabled.Value && currentItem.tag == InventoryItem.ItemTag.Food) FinishEating();
+                }
+            }
+        }
+
+        public void FinishEating()
+        {
+            UseInventory.Instance.eatSfx.Stop();
+            StartCoroutine(Play(UseInventory.Instance.eatSfx, 0.45f));
+            eatingEmission = UseInventory.Instance.eatingParticles.emission;
+            eatingEmission.enabled = false;
+            PlayerStatus.Instance.Eat(currentItem);
+            ClientSend.AnimationUpdate(OnlinePlayer.SharedAnimation.Eat, false);
+            Hotbar.Instance.UseItem(1);
+        }
+
+        public IEnumerator Play(AudioSource audioSource, float seconds)
+        {
+            audioSource.Play();
+            yield return new WaitForSeconds(seconds);
+            audioSource.Stop();
+        }
+    }
+}
---
+++ Patches.cs
@@ -0,0 +1,69 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace InstantEat.Patches
+{
+    public class MuckPatch
+    {
+        [HarmonyPrefix, HarmonyPatch(typeof(UseInventory), nameof(UseInventory.Use))]
+        public static bool OnUse()
+        {
+            if (InstantEat.currentItem && InstantEat.configInstantEatEnabled.Value) return !(InstantEat.currentItem.tag == InventoryItem.ItemTag.Food);
+            return true;
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(UseInventory), nameof(UseInventory.SetWeapon))]
+        public static void OnSetWeapon(ref InventoryItem item)
+        {
+            InstantEat.currentItem = item;
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(ChatBox), nameof(ChatBox.Instance.ChatCommand))]
+        public static void OnChatCommand(ref string message)
+        {
+            if (message == "/instanteat")
+            {
+               InstantEat.configInstantEatEnabled.Value = !InstantEat.configInstantEatEnabled.Value;
+                SendServerMessage($"Instant Eat {(InstantEat.configInstantEatEnabled.Value ? "Enabled" : "Disabled")}");
+            }
+        }
+
+        public static void SendServerMessage(string message)
+        {
+            ChatBox instance = ChatBox.Instance;
+            instance.typing = false;
+            message = TrimMessage(message);
+            if (message == "")
+            {
+                return;
+            }
+            if (message.ToCharArray()[0] == '/')
+            {
+                instance.ChatCommand(message);
+                return;
+            }
+            foreach (string input in instance.profanityList.text.Split(new char[] { '\n' }))
+            {
+                message = message.Replace(input, "muck");
+            }
+            instance.AppendMessage(0, message, "Server");
+            ClientSend.SendChatMessage(message);
+            ClearMessage();
+        }
+
+        private static void ClearMessage()
+        {
+            ChatBox.Instance.inputField.text = "";
+            ChatBox.Instance.inputField.interactable = false;
+        }
+
+        public static string TrimMessage(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                return "";
+            }
+            return message.Substring(0, Mathf.Min(message.Length, 120));
+        }
+    }
+}
